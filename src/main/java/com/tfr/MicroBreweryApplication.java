4079530c8f89845d44c0e803db2540f775bfd020
Erik Hage <ehage4@gmail.com>
2017-04-03 22:45:19 -0400

    Create Spring Initializr maven spring boot project

---
+++ src/main/java/com/tfr/MicroBreweryApplication.java
@@ -0,0 +1,12 @@
+package com.tfr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroBreweryApplication.class, args);
+	}
+}
